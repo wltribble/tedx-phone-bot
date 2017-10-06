3cde7cda6f0b62e093911f50629efad96a000342
Will Tribble <wltribble@gmail.com>
2017-10-06 18:40:28 -0500

    initial commit

---
+++ phonebot.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect
+from twilio.twiml.voice_response import Play, VoiceResponse
+
+
+app = Flask(__name__)
+
+
+@app.route('/tedx_phone_response', methods=['GET', 'POST'])
+def voice():
+    response = VoiceResponse()
+    response.play('', digits='wwww#')
+
+    return str(response)
+
+if __name__ == "__main__":
+    app.run(debug=False)
