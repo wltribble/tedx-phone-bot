15de49adef4459880c9f606621df39253fc14bd2
Will Tribble <wltribble@gmail.com>
2017-10-06 19:52:18 -0500

    wrapping response in string so maybe it will send right

--- phonebot.py
+++ phonebot.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def voice():
     response = VoiceResponse().play('', digits='wwww#')
-    return response
+    return str(response)
 
 if __name__ == "__main__":
     app.run(debug=False)
