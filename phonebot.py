54daa2f5c5056f505d935bdea5844d0a52af2ba2
Will Tribble <wltribble@gmail.com>
2017-10-06 19:40:39 -0500

    trying to fix bugs

--- phonebot.py
+++ phonebot.py
@@ -7,10 +7,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def voice():
-    response = VoiceResponse()
-    response.play('', digits='wwww#')
-
-    return str(response)
+    response = VoiceResponse().play('', digits='wwww#')
+    return response
 
 if __name__ == "__main__":
     app.run(debug=False)
